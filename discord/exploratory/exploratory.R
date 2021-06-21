03ac5698b44f14075acdc4106e450c92e44fb44d
paulapivat <paul.apivat@gmail.com>
2021-06-21 11:31:56 +0700

    add top discord channel by chatters

--- discord/exploratory/exploratory.R
+++ discord/exploratory/exploratory.R
@@ -41,6 +41,16 @@ df3 %>%
 
 # popular text channels (SINGLE DATE time-stamp)
 # arrange in descending order by number of messages
+
+# ----------- DISCORD CHANNELS ----------------#
+
+# unable to print out Emojis
+
+# Top 30 Discord Channels by Messages
+# note: duplicate - "general", one is in archive
+# note: By Date: 2021-06-08
+
+
 df4 %>%
     select(channel_name, messages) %>%
     slice(1:30) %>% 
@@ -54,9 +64,65 @@ df4 %>%
     ) +
     labs(
         title = "Top 30 Discord Channels by Messages",
-        subtitle = "Bankless DAO",
+        subtitle = "Bankless DAO (by June 8th)",
         caption = "Data: BanklessDAO | @frogmonkee | Analytics: @paulapivat",
         y = "Channels",
         x = "Number of Messages"
     )
+
+
+# Top 30 Discord Channels by Readers
+df4 %>%
+    select(channel_name, readers) %>%
+    slice(1:30) %>% 
+    arrange(desc(readers)) %>% 
+    ggplot(aes(x = readers, y = reorder(channel_name, readers), fill = channel_name)) +
+    geom_col() +
+    geom_text(aes(label = readers), hjust = -0.1) +
+    theme_minimal() +
+    theme(
+        legend.position = "none"
+    ) +
+    labs(
+        title = "Top 30 Discord Channels by Readers",
+        subtitle = "Bankless DAO (by June 8th)",
+        caption = "Data: BanklessDAO | @frogmonkee | Analytics: @paulapivat",
+        y = "Channels",
+        x = "Number of Readers"
+    )
+
+
+df4 %>%
+    select(channel_name, chatters) %>%
+    slice(1:30) %>% 
+    arrange(desc(chatters)) %>% 
+    ggplot(aes(x = chatters, y = reorder(channel_name, chatters), fill = channel_name)) +
+    geom_col() +
+    geom_text(aes(label = chatters), hjust = -0.1) +
+    theme_minimal() +
+    theme(
+        legend.position = "none"
+    ) +
+    labs(
+        title = "Top 30 Discord Channels by Chatters",
+        subtitle = "Bankless DAO (by June 8th)",
+        caption = "Data: BanklessDAO | @frogmonkee | Analytics: @paulapivat",
+        y = "Channels",
+        x = "Number of Chatters"
+    )
+
+
+# ----------------- APPENDIX ----------------- #
+
+# Try modern text features tidyverse
+emojis <- "👩🏾‍💻🔥📊"
+
+p <- ggplot() + 
+    geom_label(
+        aes(x = 0, y = 0, label = emojis), 
+        family = "Apple Color Emoji"
+    )
+
+preview_devices(p, "emoji")
+
 
