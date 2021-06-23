58ffbc9a83c7fcb68f928ce73e40644c90805ce7
paulapivat <paul.apivat@gmail.com>
2021-06-23 15:09:41 +0700

    add pagination, tokens owned per account and other details to subgraph query

--- graphql/exploratory/poap_subgraph_python.py
+++ graphql/exploratory/poap_subgraph_python.py
@@ -20,18 +20,23 @@ def run_query(q):
 
 
 # The Graph query - Query poap.xDai event where {id: 2655}
+# PAGINATION: specify, tokens(first: 200) to reconcile with CSV total 173
+# add tokensOwned ~ Power (poap.gallery)
+
 query = """
 
 {
   events(where: { id: 2655 }) {
     id
-    tokens {
+    tokens(first: 200) {
       id
       owner {
         id
+        tokensOwned
       }
       transfers {
         id
+        timestamp
         from {
           id
         }
@@ -51,3 +56,23 @@ print('Print Result - {}'.format(result))
 print('##############')
 # pretty print - printing long nested dictionaries
 pprint(result)
+
+# ensure event id is 2655
+# reconcile with poap.gallery,
+# Bankless DAO Community Call #4, event id 2655
+# csv with 173 rows (previous downloaded raw csv had 172), late claimer on June 11th, 2021
+
+# ensure event queried is id #2655
+result['data']['events'][0]['id']
+
+# list of all tokens for event id #2566
+result['data']['events'][0]['tokens']
+
+# tokens should be a list of dictionaries
+type(result['data']['events'][0]['tokens'])      # list
+type(result['data']['events'][0]['tokens'][0])   # dict
+
+# check length of list -- ideally, should match 173 rows
+# however only get 100 (need to reconcile why)
+# CONCLUSION: in above query need to specify token(first: 200) to query more than 100 results (pagination)
+len(result['data']['events'][0]['tokens'])
