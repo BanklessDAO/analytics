2021e670703cbb6ff30c31d20bbe48d7e1fa78da
paulapivat <paul.apivat@gmail.com>
2021-07-21 09:26:27 +0700

    add example schema validation for bounty-board data model

---
+++ bounty_board/schema_validation.js
@@ -0,0 +1,43 @@
+// example schema validation
+// apply to data model for bounty board
+
+db.createCollection("posts", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["title", "text", "creator", "comments"],
+      properties: {
+        title: {
+          bsonType: "string",
+          description: "must be a string and is required",
+        },
+        text: {
+          bsonType: "string",
+          description: "must be a string and is required",
+        },
+        creator: {
+          bsonType: "objectId",
+          description: "must be an objectId and is required",
+        },
+        comments: {
+          bsonType: "array",
+          description: "must be an array and is required",
+          items: {
+            bsonType: "object",
+            required: ["text", "author"],
+            properties: {
+              test: {
+                bsonType: "string",
+                description: "must be a string and is required",
+              },
+              author: {
+                bsonType: "objectId",
+                description: "must be an objectId and is required",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
