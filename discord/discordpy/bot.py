02e5a25803534ec57cd8d3482ce175d604341c82
paulapivat <paul.apivat@gmail.com>
2021-08-05 15:41:14 +0700

    add bot.py to explore discord api

---
+++ discord/discordpy/bot.py
@@ -0,0 +1,27 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+client.run(TOKEN)
