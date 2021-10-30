6577a6c146fcf69b0e461bbad2f8ebe3cf1c306a
paulapivat <paul.apivat@gmail.com>
2021-10-30 11:09:30 +0700

    update mck_bank_subgraph.py to run two requests for May 4th and 5th

--- graphql/exploratory/mck_bank_subgraph.py
+++ graphql/exploratory/mck_bank_subgraph.py
@@ -6,6 +6,8 @@ from pprint import pprint
 
 # function to use requests.posts to make API call to subgraph url
 
+# First request Tue, 04 May 2021
+
 
 def run_query(q):
     # using McKethanor Bank Subgraph Queries (HTTP)
@@ -17,19 +19,27 @@ def run_query(q):
     # NOTE 'query2'
     request = requests.post('https://api.studio.thegraph.com/query/1121/bankv1/v0.0.5'
                             '',
-                            json={'query': query2})
+                            json={'query': query})
     if request.status_code == 200:
         return request.json()
     else:
         raise Exception('Query failed. return code is {}.     {}'.format(
-            request.status_code, query2))
+            request.status_code, query))
 
-# The Graph Query
-# PAGINATION:
+
+# Second request Wed, 05 May 2021
+def run_query2(q):
+    request2 = requests.post('https://api.studio.thegraph.com/query/1121/bankv1/v0.0.5'
+                             '',
+                             json={'query': query2})
+    if request2.status_code == 200:
+        return request2.json()
+    else:
+        raise Exception('Query failed. return code is {}.     {}'.format(
+            request2.status_code, query2))
+
 
 # basic query first
-
-
 query = """
 
 {
@@ -61,7 +71,7 @@ query2 = """
 """
 
 result = run_query(query)
-result2 = run_query(query2)
+result2 = run_query2(query2)
 
 # print results
 print('Print Bank Subgraph Result - {}'.format(result))
@@ -73,23 +83,19 @@ pprint(result)
 # convert to list
 # then dig down nested layers
 
-#result_items = result.items()
-#result_list = list(result_items)
+result_items = result.items()
+result_list = list(result_items)
+lst_of_dict = result_list[0][1].get('transferBanks')
+df = pd.json_normalize(lst_of_dict)
 
-#lst_of_dict = result_list[0][1].get('transferBanks')
-
-#df = pd.json_normalize(lst_of_dict)
-
-# print(df)
+print(df)
 
 
 # repeat beyond {timestamp_gte:"1620159318"}
 
 result_items_2 = result2.items()
 result_list_2 = list(result_items_2)
-
 lst_of_dict_2 = result_list_2[0][1].get('transferBanks')
-
 df2 = pd.json_normalize(lst_of_dict_2)
 
 print(df2)
