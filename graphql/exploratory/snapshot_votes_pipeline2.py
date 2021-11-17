1be73d2d977cca5d8092da9cbbea6f48ddade894
paulapivat <paul.apivat@gmail.com>
2021-11-17 11:39:21 +0700

    update environment variable and snapshot_votes_pipeline2.py

--- graphql/exploratory/snapshot_votes_pipeline2.py
+++ graphql/exploratory/snapshot_votes_pipeline2.py
@@ -44,7 +44,7 @@ query = """
 
 # String interpolation not working
 
-#variables = {'created': max_created}
+variables = {'created': max_created}
 query2 = f"""
 {{
         votes(first: 10000, where: {{created: {max_created}}}) {{
@@ -53,9 +53,9 @@ query2 = f"""
           created
           choice
           __typename
-          proposal {
+          proposal {{
             id
-          }
+          }}
         }}
 }}
 """
@@ -88,15 +88,15 @@ mutation createVote {
 # Third string interpolation attempt
 # note: f""" yield an error
 
-variables = {
-    "input": {
-        "id": "0x0cfb9c3476157243303e1e5cef02e7719b261b566c377a1e23586915e8f604b3",
-        "voter": "0x4f8c2d5397262653Cd8956CB977A0bA3660210c7",
-        "created": "1635966410",
-        "__typename": "Vote",
-        "proposal": {"id": "0xabccf8394b35e92043a4055f8430f1babd44fdc763849ad0158441073578a62e"}
-    }
-}
+# variables = {
+#    "input": {
+#        "id": "0x0cfb9c3476157243303e1e5cef02e7719b261b566c377a1e23586915e8f604b3",
+#        "voter": "0x4f8c2d5397262653Cd8956CB977A0bA3660210c7",
+#        "created": "1635966410",
+#        "__typename": "Vote",
+#        "proposal": {"id": "0xabccf8394b35e92043a4055f8430f1babd44fdc763849ad0158441073578a62e"}
+#    }
+# }
 
 
 query4 = """
@@ -119,15 +119,15 @@ mutation createUser($input: CreateUserInput!) {
 def run_query(q):
     request = requests.post('https://hub.snapshot.org/graphql'
                             '',
-                            json={'query': query, 'variables': variables})
+                            json={'query': query2, 'variables': variables})
     if request.status_code == 200:
         return request.json()
     else:
         raise Exception('Query failed. return code is {}.     {}'.format(
-            request.status_code, query))
+            request.status_code, query2))
 
 
-result = run_query(query)
+result = run_query(query2)
 
 # print results
 print('Print Most Recent Snapshot Votes ID Result - {}'.format(result))
