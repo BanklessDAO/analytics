446e8b59de0ad001d0ccb4ffd995ba18228002c5
paulapivat <paul.apivat@gmail.com>
2021-12-19 11:27:32 +0700

    update data_request.py to include a couple endpoints

--- coordinape/data_request.py
+++ coordinape/data_request.py
@@ -1,6 +1,12 @@
 import requests
 
 # Not working: getting Response 404
-response = requests.get(
-    "https://coordinape.me/api/users?circle_id=24&deleted_users=true")
+response = requests.get("https://coordinape.me/api/users?circle_id=24")
 print(response)
+
+#response = requests.get("https://coordinape.me/api/users?circle_id=24&deleted_users=true")
+# print(response)
+
+# Not working getting Response 404
+# response = requests.get('https://staging-api.coordinape.com/api/circles')
+# print(response)
