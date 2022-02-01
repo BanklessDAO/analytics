98bb3dea149ad5722b90ce75a59b44717784b485
paulapivat <paul.apivat@gmail.com>
2022-02-01 15:56:41 +0700

    update new_bounty_queries.js

--- bounty_board/mongo/new_bounty_queries.js
+++ bounty_board/mongo/new_bounty_queries.js
@@ -1,6 +1,284 @@
-//name of people who created bounties in past 30-days
+// Weekly Active Users (past 7-days)
+
+// Number of Bounties created in past 7-days
 db.bounties.aggregate([
-  { $match: { $and: [{ season: 2 }, { customer_id: "834499078434979890" }] } },
+  { $match: { customer_id: "834499078434979890" } },
+  {
+    $project: {
+      _id: 1,
+      _customer_id: "$customer_id",
+      _title: "$title",
+      _status: "$status",
+      _createdAt: "$createdAt",
+      _claimedAt: "$claimedAt",
+      _submittedAt: "$submittedAt",
+      _reviewedAt: "$reviewedAt",
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      customer_id: "$_customer_id",
+      title: "$_title",
+      status: "$_status",
+      createdAt: { $toDate: "$_createdAt" },
+      claimedAt: { $toDate: "$_claimedAt" },
+      submittedAt: { $toDate: "$_submittedAt" },
+      reviewedAt: { $toDate: "$_reviewedAt" },
+    },
+  },
+  {
+    $match: {
+      createdAt: { $gt: new Date(new Date() - 7 * 60 * 60 * 24 * 1000) },
+    },
+  },
+]);
+
+// Number of Bounties Claimed in past 7-days
+db.bounties.aggregate([
+  { $match: { customer_id: "834499078434979890" } },
+  {
+    $project: {
+      _id: 1,
+      _customer_id: "$customer_id",
+      _title: "$title",
+      _status: "$status",
+      _createdAt: "$createdAt",
+      _claimedAt: "$claimedAt",
+      _submittedAt: "$submittedAt",
+      _reviewedAt: "$reviewedAt",
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      customer_id: "$_customer_id",
+      title: "$_title",
+      status: "$_status",
+      createdAt: { $toDate: "$_createdAt" },
+      claimedAt: { $toDate: "$_claimedAt" },
+      submittedAt: { $toDate: "$_submittedAt" },
+      reviewedAt: { $toDate: "$_reviewedAt" },
+    },
+  },
+  {
+    $match: {
+      claimedAt: { $gt: new Date(new Date() - 7 * 60 * 60 * 24 * 1000) },
+    },
+  },
+]);
+
+// Number of Bounties Submitted in past 7-days
+db.bounties.aggregate([
+  { $match: { customer_id: "834499078434979890" } },
+  {
+    $project: {
+      _id: 1,
+      _customer_id: "$customer_id",
+      _title: "$title",
+      _status: "$status",
+      _createdAt: "$createdAt",
+      _claimedAt: "$claimedAt",
+      _submittedAt: "$submittedAt",
+      _reviewedAt: "$reviewedAt",
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      customer_id: "$_customer_id",
+      title: "$_title",
+      status: "$_status",
+      createdAt: { $toDate: "$_createdAt" },
+      claimedAt: { $toDate: "$_claimedAt" },
+      submittedAt: { $toDate: "$_submittedAt" },
+      reviewedAt: { $toDate: "$_reviewedAt" },
+    },
+  },
+  {
+    $match: {
+      submittedAt: { $gt: new Date(new Date() - 7 * 60 * 60 * 24 * 1000) },
+    },
+  },
+]);
+
+// Number of Bounties Reviewed in past 7-days
+db.bounties.aggregate([
+  { $match: { customer_id: "834499078434979890" } },
+  {
+    $project: {
+      _id: 1,
+      _customer_id: "$customer_id",
+      _title: "$title",
+      _status: "$status",
+      _createdAt: "$createdAt",
+      _claimedAt: "$claimedAt",
+      _submittedAt: "$submittedAt",
+      _reviewedAt: "$reviewedAt",
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      customer_id: "$_customer_id",
+      title: "$_title",
+      status: "$_status",
+      createdAt: { $toDate: "$_createdAt" },
+      claimedAt: { $toDate: "$_claimedAt" },
+      submittedAt: { $toDate: "$_submittedAt" },
+      reviewedAt: { $toDate: "$_reviewedAt" },
+    },
+  },
+  {
+    $match: {
+      reviewedAt: { $gt: new Date(new Date() - 7 * 60 * 60 * 24 * 1000) },
+    },
+  },
+]);
+
+// Monthly Active Users (past 30-days)
+
+//Number of Bounties created in past 30-days
+db.bounties.aggregate([
+  { $match: { customer_id: "834499078434979890" } },
+  {
+    $project: {
+      _id: 1,
+      _customer_id: "$customer_id",
+      _title: "$title",
+      _status: "$status",
+      _createdAt: "$createdAt",
+      _claimedAt: "$claimedAt",
+      _submittedAt: "$submittedAt",
+      _reviewedAt: "$reviewedAt",
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      customer_id: "$_customer_id",
+      title: "$_title",
+      status: "$_status",
+      createdAt: { $toDate: "$_createdAt" },
+      claimedAt: { $toDate: "$_claimedAt" },
+      submittedAt: { $toDate: "$_submittedAt" },
+      reviewedAt: { $toDate: "$_reviewedAt" },
+    },
+  },
+  {
+    $match: {
+      createdAt: { $gt: new Date(new Date() - 30 * 60 * 60 * 24 * 1000) },
+    },
+  },
+]);
+
+// Number of Bounties claimed in past 30-days
+db.bounties.aggregate([
+  { $match: { customer_id: "834499078434979890" } },
+  {
+    $project: {
+      _id: 1,
+      _customer_id: "$customer_id",
+      _title: "$title",
+      _status: "$status",
+      _createdAt: "$createdAt",
+      _claimedAt: "$claimedAt",
+      _submittedAt: "$submittedAt",
+      _reviewedAt: "$reviewedAt",
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      customer_id: "$_customer_id",
+      title: "$_title",
+      status: "$_status",
+      createdAt: { $toDate: "$_createdAt" },
+      claimedAt: { $toDate: "$_claimedAt" },
+      submittedAt: { $toDate: "$_submittedAt" },
+      reviewedAt: { $toDate: "$_reviewedAt" },
+    },
+  },
+  {
+    $match: {
+      claimedAt: { $gt: new Date(new Date() - 30 * 60 * 60 * 24 * 1000) },
+    },
+  },
+]);
+
+// Number of Bounties Submitted in past 30-days
+db.bounties.aggregate([
+  { $match: { customer_id: "834499078434979890" } },
+  {
+    $project: {
+      _id: 1,
+      _customer_id: "$customer_id",
+      _title: "$title",
+      _status: "$status",
+      _createdAt: "$createdAt",
+      _claimedAt: "$claimedAt",
+      _submittedAt: "$submittedAt",
+      _reviewedAt: "$reviewedAt",
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      customer_id: "$_customer_id",
+      title: "$_title",
+      status: "$_status",
+      createdAt: { $toDate: "$_createdAt" },
+      claimedAt: { $toDate: "$_claimedAt" },
+      submittedAt: { $toDate: "$_submittedAt" },
+      reviewedAt: { $toDate: "$_reviewedAt" },
+    },
+  },
+  {
+    $match: {
+      submittedAt: { $gt: new Date(new Date() - 30 * 60 * 60 * 24 * 1000) },
+    },
+  },
+]);
+
+// Number of Bounties Reviewed in past 30-days
+db.bounties.aggregate([
+  { $match: { customer_id: "834499078434979890" } },
+  {
+    $project: {
+      _id: 1,
+      _customer_id: "$customer_id",
+      _title: "$title",
+      _status: "$status",
+      _createdAt: "$createdAt",
+      _claimedAt: "$claimedAt",
+      _submittedAt: "$submittedAt",
+      _reviewedAt: "$reviewedAt",
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      customer_id: "$_customer_id",
+      title: "$_title",
+      status: "$_status",
+      createdAt: { $toDate: "$_createdAt" },
+      claimedAt: { $toDate: "$_claimedAt" },
+      submittedAt: { $toDate: "$_submittedAt" },
+      reviewedAt: { $toDate: "$_reviewedAt" },
+    },
+  },
+  {
+    $match: {
+      reviewedAt: { $gt: new Date(new Date() - 30 * 60 * 60 * 24 * 1000) },
+    },
+  },
+]);
+
+// POWER USERS: Number of People who created, claimed, submitted, reviewed bounties in past 30-days
+
+//People who Created bounties in past 30-days
+db.bounties.aggregate([
+  { $match: { customer_id: "834499078434979890" } },
   {
     $project: {
       _id: 1,
@@ -28,9 +306,9 @@ db.bounties.aggregate([
   },
 ]);
 
-//name of people who claimed bounties in past 30-days
+//People who Claimed bounties in past 30-days
 db.bounties.aggregate([
-  { $match: { $and: [{ season: 2 }, { customer_id: "834499078434979890" }] } },
+  { $match: { customer_id: "834499078434979890" } },
   {
     $project: {
       _id: 1,
@@ -60,7 +338,7 @@ db.bounties.aggregate([
 
 //name of people who submitted bounties in past 30-days
 db.bounties.aggregate([
-  { $match: { $and: [{ season: 2 }, { customer_id: "834499078434979890" }] } },
+  { $match: { customer_id: "834499078434979890" } },
   {
     $project: {
       _id: 1,
@@ -90,7 +368,7 @@ db.bounties.aggregate([
 
 //name of people who reviewed bounties in past 30-days
 db.bounties.aggregate([
-  { $match: { $and: [{ season: 2 }, { customer_id: "834499078434979890" }] } },
+  { $match: { customer_id: "834499078434979890" } },
   {
     $project: {
       _id: 1,
